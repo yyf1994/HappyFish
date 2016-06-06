9f28293da55196139e4cc9516c4ff2752e30246e
yyf1994 <1031463226@qq.com>
2016-06-06 18:11:57 +0800

    设置背景
    设置背景

--- app/src/main/java/com/yyf/happyfish/wechat/adapter/WeChatAdapter.java
+++ app/src/main/java/com/yyf/happyfish/wechat/adapter/WeChatAdapter.java
@@ -11,7 +11,6 @@ import com.yyf.happyfish.wechat.model.ListEntity;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 /**
  * Created by Administrator on 2016/3/18.
@@ -20,31 +19,20 @@ import java.util.Random;
 public class WeChatAdapter extends BaseQuickAdapter<ListEntity> {
     ArrayList<String> color;
 
-    public WeChatAdapter(Context context, List<ListEntity> datas,ArrayList<String> color) {
-        super(context, R.layout.wechat_item, datas);
+    public WeChatAdapter(Context context, List<ListEntity> datas, ArrayList<String> color) {
+        super(context, R.layout.wechat_item_threebg, datas);
         this.color = color;
     }
 
     @Override
     protected void convert(BaseViewHolder helper, ListEntity item) {
-        Random mRandom = new Random();
-        int a = mRandom.nextInt(color.size());
-        String s = color.get(a);
-        System.out.println(s + "");
-//        color.remove(a);
-//        for(String s:color){
-            if(s.equals("red")){
-                helper.setBackgroundRes(R.id.cardview,R.mipmap.hongbao1);
-            }else if(s.equals("purple")){
-                helper.setBackgroundRes(R.id.cardview,R.mipmap.hoongbao2);
-            }else{
-                helper.setBackgroundRes(R.id.cardview,R.mipmap.hongbao3);
-            }
-//        }
+        helper.setBackgroundRes(R.id.item1, R.mipmap.hongbao1);
+        helper.setBackgroundRes(R.id.item2, R.mipmap.hoongbao2);
+        helper.setBackgroundRes(R.id.item3, R.mipmap.hongbao3);
 
         helper.setText(R.id.title_tv, item.getTitle())
-        .setText(R.id.source_tv,"来自"+item.getSource())
-        .setImageUrl(R.id.img_firstimg,item.getFirstImg(),R.mipmap.ic_launcher,new GlideCircleTransform(mContext));
+                .setText(R.id.source_tv, "来自" + item.getSource())
+                .setImageUrl(R.id.img_firstimg, item.getFirstImg(), R.mipmap.ic_launcher, new GlideCircleTransform(mContext));
               /*  .setText(R.id.tv_miaoshu, item.getAvatar())
                 .setText(R.id.tv_time, item.getName())
                 .setImageUrl(R.id.img_logo, item.getAvatar(), R.mipmap.ic_launcher, new GlideCircleTransform(mContext))
